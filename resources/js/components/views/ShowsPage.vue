818745e5e3ef531209d8de3ac5baf12b0e3083e5
Audrius <aubuka@gmail.com>
2019-01-06 15:55:48 +0000

    show past and future shows

--- resources/js/components/views/ShowsPage.vue
+++ resources/js/components/views/ShowsPage.vue
@@ -14,8 +14,14 @@
         <div v-else>
 
           <div v-if="shows.data.length">
+            <h3 v-if="future_shows.length" class="show-heading">Future shows</h3>
             <div class="box">
-              <show v-for="show in shows.data" :key="show.id" :show="show"></show>
+              <show v-for="show in future_shows" :key="show.id" :show="show"></show>
+            </div>
+
+            <h3 v-if="past_shows.length" class="show-heading">Past shows</h3>
+            <div class="box">
+              <show v-for="show in past_shows" :key="show.id" :show="show"></show>
             </div>
 
             <div class="columns">
@@ -46,6 +52,8 @@
       return {
         loading: 0,
         shows: [],
+        future_shows: [],
+        past_shows: [],
         page: 1,
         limit: 10,
         placeholderRows: [
@@ -85,9 +93,24 @@
       next: function() {
         this.page++;
       },
+
       prev: function() {
         this.page--;
-      }
+      },
+    },
+
+    watch: {
+      shows: function (val) {
+        var context = this;
+        val.data.forEach(function(elem) {
+          if (moment(elem.date).isAfter()) {
+            context.future_shows.push(elem);
+          }
+          else {
+            context.past_shows.push(elem);
+          }
+        });
+      },
     },
 
     components: {
